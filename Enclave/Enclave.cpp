967e7394ec06f10890029de74895c6063b63a52f
cozyBoi <kjjh1126@sogang.ac.kr>
2020-07-10 15:52:25 +0900

    write sealed data into file

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -83,7 +83,7 @@ void printf_helloworld(char policy_arr[32][1000 + sizeof(sgx_sealed_data_t)], in
     uint32_t plaintext_len = 1000;
     uint32_t sealed_size = sgx_calc_sealed_data_size(NULL, 4);
     printf("sealed_size : %d\n", sealed_size);
-    char sealed_data[sizeof(sgx_sealed_data_t) + 1000];
+    char sealed_data[sealed_size];
     
     
     sgx_seal_data(0, NULL,plaintext_len, (uint8_t*)tmp_policy, sealed_size, (sgx_sealed_data_t*)sealed_data);
